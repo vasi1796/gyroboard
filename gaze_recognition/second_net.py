2df3e43dc3d4f67825555290e4e8ae9bf864f9ff
andrei.home <vasy1796@gmail.com>
2018-02-07 12:39:02 +0200

    Finished gaze recognition network

--- gaze_recognition/second_net.py
+++ gaze_recognition/second_net.py
@@ -17,11 +17,12 @@ def load_data(dataset):
 
 
 batch_size = 32  # in each iteration, we consider 32 training examples at once
-num_epochs = 50  # we iterate 200 times over the entire training set
+num_epochs = 20  # we iterate 200 times over the entire training set
 kernel_size = 3  # we will use 3x3 kernels throughout
 pool_size = 2  # we will use 2x2 pooling throughout
 conv_depth_1 = 32  # we will initially have 32 kernels per conv. layer...
 conv_depth_2 = 64  # ...switching to 64 after the first pooling layer
+drop_prob_1 = 0.25
 drop_prob_2 = 0.5  # dropout in the FC layer with probability 0.5
 hidden_size = 512  # the FC layer will have 512 neurons
 
@@ -44,14 +45,15 @@ conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same',
 pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_1)
 conv_2 = Convolution2D(conv_depth_1, (kernel_size, kernel_size), padding='same', activation='relu')(pool_1)
 pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
-conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(pool_2)
+drop_1 = Dropout(drop_prob_1)(pool_2)
+conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size), padding='same', activation='relu')(drop_1)
 pool_3 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_3)
 flat = Flatten()(pool_3)
 hidden = Dense(hidden_size, activation='relu')(flat)
 drop_4 = Dropout(drop_prob_2)(hidden)
 out = Dense(num_classes, activation='softmax')(drop_4)
 
-tbCallback = TensorBoard(log_dir='./Graph', histogram_freq=25, write_graph=True, write_images=True)
+tbCallback = TensorBoard(log_dir='./Graph', histogram_freq=10, write_graph=True, write_images=True)
 
 model = Model(inputs=inp, outputs=out)  # To define a model, just specify its input and output layers
 
