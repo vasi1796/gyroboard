430ef18687b4347bba8560038eca9804435bf252
andrei.home <vasy1796@gmail.com>
2018-03-10 15:50:04 +0200

    Redone pyqt with labels and interfaced arduino

--- project_scripts/test_viz.py
+++ project_scripts/test_viz.py
@@ -1,47 +1,60 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QGridLayout,
-                             QPushButton, QApplication)
-from PyQt5.QtGui import QKeyEvent
+from PyQt5 import QtWidgets, QtCore
+import datetime
+import numpy as np
+import serial
 
-
-class Example(QWidget):
-
+class keyboard_scroll(object):
     def __init__(self):
-        super().__init__()
-        self.initUI()
+        self.label = QtWidgets.QLabel(Form)
+        self.labels = []
 
-    def initUI(self):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 400)
+        for index in range(0,5):
+            label=QtWidgets.QLabel(Form)
+            label.setGeometry(QtCore.QRect(80+50*index, 80, 40+50*index, 20))
+            self.labels.append(label)
+        for index in range(0,5):
+            label=QtWidgets.QLabel(Form)
+            if index == 2:
+                continue
+            else:
+                label.setGeometry(QtCore.QRect(180, 20+20*index, 40, 20+20*index))
+            self.labels.append(label)
 
-        grid = QGridLayout()
-        self.setLayout(grid)
+        self.label.setGeometry(QtCore.QRect(180, 200, 60, 20))
+        self.label.setObjectName("label")
 
-        keyboard = "abcdefghijklmnopqrstuvwxyz1234567890"
-        letter_list = []
-        for letter in keyboard:
-            letter_list.append(letter)
-        print(len(letter_list))
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
-        positions_a = [(2, j) for j in range(5)]
-        positions_b = [(i, 2) for i in range(5)]
-        positions = positions_a + positions_b
-        for index in range(0,len(positions)):
-            button = QPushButton(keyboard[index])
-            grid.addWidget(button, *positions[index])
-
-        self.move(300, 150)
-        self.setWindowTitle('Keyboard')
-        self.show()
-
-    def keyPressEvent(self, event):
-        if type(event) == QKeyEvent:
-            # here accept the event and do something
-            print(event.key())
-            event.accept()
-        else:
-            event.ignore()
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "init clock"))
+        for index in range(0,len(self.labels)):
+            self.labels[index].setText(_translate("Form", "hello"+str(index)))
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = keyboard_scroll()
+    ui.setupUi(Form)
+    Form.show()
+    ser = serial.Serial('\\.\COM6', 115200)
+    ser.close()
+    ser.open()
+
+    def update_label():
+        current_time = str(datetime.datetime.now().time())
+        ui.label.setText(current_time)
+        print(ser.readline())
+
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update_label)
+    timer.start(500)  # every 10,000 milliseconds
+
     sys.exit(app.exec_())
