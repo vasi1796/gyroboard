9f80c3cbd304c70084b70ec5ee7f2c653930b464
andrei.home <vasy1796@gmail.com>
2018-03-25 01:28:04 +0200

    Renamed functions and fixed some movements in the slider

--- project_scripts/test_viz.py
+++ project_scripts/test_viz.py
@@ -1,7 +1,6 @@
 import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
 import datetime
-import numpy as np
 import serial
 import re
 from threading import Thread
@@ -9,14 +8,14 @@ from threading import Thread
 string_vect = []
 
 
-def processSerialString(serial_object):
+def process_serial_string(serial_object):
     while True:
         angle_string = serial_object.readline().decode().strip('\t\r\n')
         global string_vect
         string_vect = re.split(r'\t+', angle_string)
 
 
-class keyboard_scroll(object):
+class KeyboardScroll(object):
     def __init__(self):
         self.label = QtWidgets.QLabel(Form)
         self.labels = []
@@ -28,7 +27,7 @@ class keyboard_scroll(object):
             self.letter_list.append(letter)
         print(len(self.letter_list))
 
-    def setupUi(self, Form):
+    def setup_ui(self, Form):
         Form.setObjectName("Form")
         Form.resize(400, 400)
         for index in range(0, 5):
@@ -46,17 +45,17 @@ class keyboard_scroll(object):
         self.label.setGeometry(QtCore.QRect(180, 200, 60, 20))
         self.label.setObjectName("label")
 
-        self.retranslateUi(Form)
+        self.retranslate_ui(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
-    def retranslateUi(self, Form):
+    def retranslate_ui(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.label.setText(_translate("Form", "init clock"))
         for index in range(0, len(self.labels)):
             self.labels[index].setText(_translate("Form", self.letter_list[index]))
 
-    def processAngles(self, angles_string):
+    def process_angles(self, angles_string):
         roll_kalman = float(angles_string[2])
         yaw_kalman = float(angles_string[5])
         if roll_kalman > 20:
@@ -70,7 +69,7 @@ class keyboard_scroll(object):
         else:
             return 5
 
-    def moveList(self, index, direction):
+    def move_list(self, index, direction):
         if (direction is "up") or (direction is "right"):
             if index is 35:
                 index = 0
@@ -95,26 +94,30 @@ class keyboard_scroll(object):
             if direction is "down":
                 for label_index in [5, 6, 2, 7, 8]:
                     self.labels[label_index].setText(self.letter_list[localIndex])
-                    localIndex -= 1
+                    if localIndex is 35:
+                        localIndex = -1
+                    localIndex += 1
             else:
                 for label_index in range(5):
                     self.labels[label_index].setText(self.letter_list[localIndex])
-                    localIndex -= 1
+                    if localIndex is 35:
+                        localIndex = -1
+                    localIndex += 1
             index -= 1
         return index
 
-    def updateLabels(self, angles_string):
-        movement = self.processAngles(angles_string)
+    def update_labels(self, angles_string):
+        movement = self.process_angles(angles_string)
         if movement is 1:
-            self.verticalIndex = self.moveList(self.verticalIndex, "up")
+            self.verticalIndex = self.move_list(self.verticalIndex, "up")
         elif movement is 2:
-            self.verticalIndex = self.moveList(self.verticalIndex, "down")
+            self.verticalIndex = self.move_list(self.verticalIndex, "down")
         elif movement is 3:
-            self.horizontalIndex = self.moveList(self.horizontalIndex, "right")
+            self.horizontalIndex = self.move_list(self.horizontalIndex, "right")
         elif movement is 4:
-            self.horizontalIndex = self.moveList(self.horizontalIndex, "left")
+            self.horizontalIndex = self.move_list(self.horizontalIndex, "left")
 
-    def getCenterLabel(self):
+    def get_center_label(self):
         doc = QtGui.QTextDocument()
         doc.setHtml(self.labels[2].text())
         labelText = doc.toPlainText()
@@ -124,13 +127,13 @@ class keyboard_scroll(object):
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = keyboard_scroll()
-    ui.setupUi(Form)
+    ui = KeyboardScroll()
+    ui.setup_ui(Form)
     Form.show()
     ser = serial.Serial('\\.\COM8', 115200)
     ser.close()
     ser.open()
-    thread = Thread(target=processSerialString, args=(ser,))
+    thread = Thread(target=process_serial_string, args=(ser,))
     thread.start()
 
 
@@ -140,7 +143,7 @@ if __name__ == "__main__":
         global string_vect
         if len(string_vect) is 6:
             temp = string_vect
-            ui.updateLabels(temp)
+            ui.update_labels(temp)
 
 
     timer = QtCore.QTimer()
